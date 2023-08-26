0521565187f5054ed80ba5e3d3d81f76278acfe8
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-08-26 22:47:09 +0600

    database connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,35 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mc9s3rj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const servicesCollection = client.db("luxuryLiving").collection("services");
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const services = await servicesCollection.find(query).toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
